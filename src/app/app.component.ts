f009072ded8ac122fc3b439ddfa56a65ac23fe68
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-04-16 21:03:09 +0300

    component table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-introduction';
+  name = 'Eva'
+  person = {
+    givenName: 'Eva',
+    surName: 'Peristeri',
+    age: 36,
+    email:"eva_88_bio@hotmail.com"
+  }
 }
