070a976e0c2c2be93d6ecf96f89cda0a5c8a0085
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-04-29 21:02:03 +0300

    output component & material design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { ForDirectiveExampleComponent } from './components/for-directive-example
 import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
 import { ComponentInputExampleComponent } from './components/component-input-example/component-input-example.component';
 import { SimpleDatatableExampleComponent } from './components/simple-datatable-example/simple-datatable-example.component';
+import { ComponentOutputExampleComponent } from './components/component-output-example/component-output-example.component';
+import { TemplateDrivenFormExampleComponent } from './components/template-driven-form-example/template-driven-form-example.component';
 
 export const routes: Routes = [
   {path:'welcome', component: WelcomeComponent},
@@ -11,5 +13,7 @@ export const routes: Routes = [
   {path:'event-bind-example', component: EventBindExampleComponent},
   {path:'component-input-example', component: ComponentInputExampleComponent},
   {path:'simple-datatable-example', component: SimpleDatatableExampleComponent},
+  {path: 'component-output-example', component: ComponentOutputExampleComponent},
+  {path:'template-driven-form-example', component:TemplateDrivenFormExampleComponent},
   {path:'', redirectTo:'/welcome', pathMatch:'full'}
 ];
